e00c995d656e9718665292d927f22a633174e991
chucknerd <chucknerd1@gmail.com>
2019-12-08 10:16:29 -0600

    adding javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+const insults = [
+    "I can only explain it to you. I can't understand it for you",
+    "I envy everyone you have never met.",
+    "You're not pretty enough to be that stupid.",
+    "I would love to insult you, but I'm afraid I won't do it as well as nature did.",
+    "I have neither the time, nor the crayons, to explain this to you.",
+];
+
+document.addEventListener("click");
+
+
+
