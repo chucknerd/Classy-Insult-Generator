d4085dcaf9d5e4c24e876c837ecd1c63ad7c9690
chucknerd <chucknerd1@gmail.com>
2019-12-08 13:00:05 -0600

    FINAL PUSH

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ document.getElementById("button").addEventListener("click", () => {
     const insults = [
         "I can only explain it to you. I can't understand it for you.",
         "I envy everyone you have never met.",
-        "You're not pretty enough to be that stupid.",
         "I would love to insult you, but I'm afraid I won't do it as well as nature did.",
         "I have neither the time, nor the crayons, to explain this to you.",
         "You spit in the face of evolution.",
-
+        "Your mother must have been of questionable birth.",
+        "It's not your fault. Blame your parents.",
+        "Bless your heart.",
+        "If you have to ask, then you already know.",
     ];
 
     function randomQuote() {
